d32ac21db8e541fcc78b3caf921bb12f9902c0a0
mirkuriit <93356764+mirkuriit@users.noreply.github.com>
2021-10-29 11:55:17 +0800

    Create Dots.java

---
+++ Dots.java
@@ -0,0 +1,43 @@
+package com.company.homework1;
+ 
+public class Dots {
+    private double x, y;
+    public Dots(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+ 
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+ 
+    public double getStart_to_Dot(){
+        double x = this.x;
+        double y = this.y;
+        return Math.sqrt(x*x + y*y);
+    }
+ 
+    public double getDot_to_Dot(Dots first, Dots second){
+        double x1 = first.getX(), y1 = first.getY();
+        double x2 = second.getX(), y2 = second.getY();
+        return Math.sqrt((x2 -x1)*(x2-x1) + (y2 -y1)*(y2-y1));
+    }
+ 
+    public double[] getMidpoint(Dots first, Dots second){
+        double x1 = first.getX(), y1 = first.getY();
+        double x2 = second.getX(), y2 = second.getY();
+        double[] a = new double[2];
+        a[0] = (x1+x2)/2;
+        a[1] = (y1+y2)/2;
+        return a;
+    }
+}
