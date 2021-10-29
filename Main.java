9fdb478377a97b7b30df0e2dd11a27dec189dd15
mirkuriit <93356764+mirkuriit@users.noreply.github.com>
2021-10-29 12:11:02 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,6 @@ public class Main {
     static Scanner in = new Scanner(System.in);
  
     public static void main(String[] args){
-        /*double a = firstDot.start_to_Dot();
-        System.out.printf("%.2f", a);*/
- 
         while (true){
  
             System.out.println("Введите цифру для выбора одного из возможных действий:");
@@ -17,15 +14,18 @@ public class Main {
             System.out.println("3.Посчитать координаты середины отрезка, образованного двумя заданными точками");
             System.out.println("4.Выход и завершение программы");
             int answer = in.nextInt();
+         
             if (answer == 1){
                 Dots firstDot = new Dots(in.nextDouble(), in.nextDouble());
                 System.out.printf("%.2f", firstDot.getStart_to_Dot());
             }
+         
             else if (answer == 2){
                 Dots firstDot = new Dots(in.nextDouble(), in.nextDouble());
                 Dots secondDot = new Dots(in.nextDouble(), in.nextDouble());
                 System.out.printf("%.2f", firstDot.getDot_to_Dot(firstDot, secondDot));
             }
+         
             else if (answer == 3){
                 Dots firstDot = new Dots(in.nextDouble(), in.nextDouble());
                 Dots secondDot = new Dots(in.nextDouble(), in.nextDouble());
@@ -34,6 +34,7 @@ public class Main {
                     System.out.println(x + " ");
                 }
             }
+         
             else if (answer == 4){
                 break;
             }
